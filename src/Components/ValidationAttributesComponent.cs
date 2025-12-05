cfc334943b77b568e3bb0fc0532c82860ee7d772
Søren Kottal <sk@ecreo.dk>
2025-12-05 08:16:03 +0100

    Port to v17
    Also swaps IComponent with IAsyncComponent as IComponent is obsolete in v17

--- src/Components/ValidationAttributesComponent.cs
+++ src/Components/ValidationAttributesComponent.cs
@@ -1,27 +1,37 @@
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Our.Umbraco.ValidationAttributes.Services;
 using Umbraco.Cms.Core.Composing;
-using Umbraco.Cms.Core.Dictionary;
-using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Web.Common;
 
 namespace Our.Umbraco.ValidationAttributes.Components
 {
     public class ValidationAttributesComposer : ComponentComposer<ValidationAttributesComponent> { }
-    public class ValidationAttributesComponent : IComponent
+
+    public class ValidationAttributesComponent : IAsyncComponent
     {
         public IUmbracoHelperAccessor _umbracoHelperAccessor;
         public IConfiguration _configuration;
+
         public ValidationAttributesComponent(
             IUmbracoHelperAccessor umbracoHelperAccessor,
             IConfiguration configuration
-            )
+        )
         {
             _umbracoHelperAccessor = umbracoHelperAccessor;
             _configuration = configuration;
         }
 
-        public void Initialize() => ValidationAttributesService.Start(_umbracoHelperAccessor, _configuration);
-        public void Terminate() {}
+        public Task InitializeAsync(bool isRestarting, CancellationToken cancellationToken)
+        {
+            ValidationAttributesService.Start(_umbracoHelperAccessor, _configuration);
+            return Task.CompletedTask;
+        }
+
+        public Task TerminateAsync(bool isRestarting, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
